e528914c47fe2f7d526e51c79e06a3c5861024c7
klrc <1440698245@qq.com>
2019-12-25 12:56:46 +0800

    change lr

--- src/recurrent_attention_network_paper/pretrain_mobilenet.py
+++ src/recurrent_attention_network_paper/pretrain_mobilenet.py
@@ -56,7 +56,7 @@ def run():
     cudnn.benchmark = True
 
     criterion = torch.nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=0.05, momentum=0.9)
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
     trainset = CUB200_loader('external/CUB_200_2011', split='train')
     testset = CUB200_loader('external/CUB_200_2011', split='test')
@@ -65,7 +65,7 @@ def run():
 
     classes = [line.split(' ')[1] for line in open('external/CUB_200_2011/classes.txt', 'r').readlines()]
 
-    for epoch in range(400):  # loop over the dataset multiple times
+    for epoch in range(50):  # loop over the dataset multiple times
         losses = 0
         for step, (inputs, labels) in enumerate(trainloader, 0):
             inputs, labels = Variable(inputs).cuda(), Variable(labels).cuda()
@@ -83,7 +83,7 @@ def run():
                 log(f':: loss @step({step:2d}/{len(trainloader)})-epoch{epoch}: {loss:.10f}\tavg_loss_20: {avg_loss:.10f}')
                 losses = 0
         eval(net, testloader)
-        if epoch % 50 == 0 and epoch != 0:
+        if epoch % 5 == 0 and epoch != 0:
             stamp = f'e{epoch}{int(time.time())}'
             torch.save(net, f'build/mobilenet_v2_cub200-{stamp}.pt')
             torch.save(optimizer.state_dict, f'build/optimizer-{stamp}.pt')
