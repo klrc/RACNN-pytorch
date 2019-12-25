b963603adc88899b4f5ae2417917be8531922106
klrc <1440698245@qq.com>
2019-12-25 17:12:14 +0800

    cuda_device=1

--- src/recurrent_attention_network_paper/forge.py
+++ src/recurrent_attention_network_paper/forge.py
@@ -15,6 +15,7 @@ from src.recurrent_attention_network_paper.CUB_loader import CUB200_loader
 from src.recurrent_attention_network_paper.pretrain_apn import random_sample
 from torch.autograd import Variable
 
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 def avg(x): return sum(x)/len(x)
 
